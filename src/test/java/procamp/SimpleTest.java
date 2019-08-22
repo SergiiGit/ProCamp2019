f3ef210e599e9b820fe27f479d011a1bc8b8c79c
SergiiIavt <Sergii.Iavtushkevych@globallogic.com>
2019-08-22 19:12:20 +0300

    Initial commit

---
+++ src/test/java/procamp/SimpleTest.java
@@ -0,0 +1,12 @@
+package procamp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SimpleTest {
+
+    @Test
+    public void test() {
+        Assert.assertEquals(5+5,11);
+    }
+}
