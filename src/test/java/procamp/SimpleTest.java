0eca7caea8a56ccdaf04dd843a6f2b71bfa1877c
SergiiIavt <Sergii.Iavtushkevych@globallogic.com>
2019-08-22 19:35:37 +0300

    Add selenium

--- src/test/java/procamp/SimpleTest.java
+++ src/test/java/procamp/SimpleTest.java
@@ -1,12 +1,32 @@
 package procamp;
 
-import org.junit.Assert;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SimpleTest {
 
+    private WebDriver driver;
+
+    @Before
+    public void startBrowser()
+    {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void stopBrowser()
+    {
+        driver.quit();
+    }
+
     @Test
-    public void test() {
-        Assert.assertEquals(5+5,11);
+    public void test()
+    {
+        driver.get("https://www.seleniumhq.org/");
     }
 }
